ad251b5775d54f9556bcbcc20e1d78ab5511e2b3
FarhanZizz <iamfarhanaziz@gmail.com>
2023-06-13 13:03:49 +0600

    fixed card image issue

--- src/Components/MeetupCard.js
+++ src/Components/MeetupCard.js
@@ -3,14 +3,14 @@ import Link from "next/link"
 const MeetupCard = ({ meetup }) => {
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl rounded">
-      <figure>
+      <figure className="flex-1">
         <img
-          className="object-cover h-72 w-80 "
+          className="object-cover h-72 w-80"
           src={meetup.image}
           alt="Album"
         />
       </figure>
-      <div className="card-body ">
+      <div className="card-body flex-1">
         <h2 className="card-title">{meetup.name}</h2>
         <p>{meetup.description}</p>
         <div className="card-actions justify-center">
